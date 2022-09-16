62902d0f4358de4c7085bbb750a5c0fb898d2729
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-09-16 10:15:16 +0000

    [add]Task_3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,25 @@ class BooksController < ApplicationController
     end
 
     def index
+      @book = Book.new
+      @books = Book.all
+
     end
 
     def edit
+    end
+
+    def create
+        book = Book.find(params[:current_user])
+        book.save(parama_path)
+        redirect_to books_path
+
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :body)
     end
 
 end
