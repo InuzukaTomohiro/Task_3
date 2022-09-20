60a4d1aa028822dfe99092e86260e59cfb4e3292
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-09-20 07:39:03 +0000

    [add]Task_3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+    before_action :correct_user, only: [:edit, :update]
 
     def index
         @users = User.all
+        @book = Book.new
     end
 
     def show
         @user = User.find(params[:id])
         @books = @user.books
+        @book = Book.new
     end
 
     def edit
@@ -27,6 +30,11 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:profile_image, :name, :introduction)
+    end
+
+    def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path(current_user.id)) unless @user == current_user
     end
 
 end
